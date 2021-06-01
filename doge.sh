ec145290517ed039d6e72287be2369d08e2501f3
whitekopi5758 <85154204+whitekopi5758@users.noreply.github.com>
2021-06-01 14:28:32 +0700

    Update doge.sh

--- doge.sh
+++ doge.sh
@@ -2,4 +2,4 @@
 sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
 
-./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DC55Ecrb7DgdV7frC3cvnA4opcWrzHjbvS.whitekopi -p x -k -a rx/0
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DC55Ecrb7DgdV7frC3cvnA4opcWrzHjbvS.whitekopi -p x -k -a rx/0
